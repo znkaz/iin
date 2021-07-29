aead7123f111d874320479a089601a1c7b24bc46
Vitaliy <theyamshikov@yandex.ru>
2021-07-29 18:56:17 +0600

    upd

--- src/Domain/Libs/CheckSum.php
+++ src/Domain/Libs/CheckSum.php
@@ -11,7 +11,7 @@ class CheckSum
     private $sequences = [
         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
         [10, 11, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-        //[3, 4, 5, 6, 7, 8, 9, 10, 11, 1, 2],
+        [3, 4, 5, 6, 7, 8, 9, 10, 11, 1, 2],
     ];
     
     public function generateSum(string $inn): CheckSumEntity
