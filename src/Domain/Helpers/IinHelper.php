90c5dcaa307076bb12f04d324997ccfe6256cbec
ZnDeveloper <zndev@ya.ru>
2022-06-19 22:26:58 +0600

    upd

--- src/Domain/Helpers/IinHelper.php
+++ src/Domain/Helpers/IinHelper.php
@@ -2,7 +2,8 @@
 
 namespace ZnKaz\Iin\Domain\Helpers;
 
-use ZnCore\Base\Libs\Text\Helpers\StringHelper;
+
+use ZnCore\Base\Libs\Text\Helpers\TextHelper;
 use ZnKaz\Iin\Domain\Entities\IndividualEntity;
 use ZnKaz\Iin\Domain\Libs\CheckSum;
 
@@ -18,11 +19,11 @@ class IinHelper
     public static function entityToString(IndividualEntity $individualEntity): string
     {
         $iin =
-            StringHelper::fill($individualEntity->getBirthday()->getDecade(), 2, '0', 'before') .
-            StringHelper::fill($individualEntity->getBirthday()->getMonth(), 2, '0', 'before') .
-            StringHelper::fill($individualEntity->getBirthday()->getDay(), 2, '0', 'before') .
+            TextHelper::fill($individualEntity->getBirthday()->getDecade(), 2, '0', 'before') .
+            TextHelper::fill($individualEntity->getBirthday()->getMonth(), 2, '0', 'before') .
+            TextHelper::fill($individualEntity->getBirthday()->getDay(), 2, '0', 'before') .
             $individualEntity->getCentury() .
-            StringHelper::fill($individualEntity->getSerialNumber(), 4, '0', 'before');
+            TextHelper::fill($individualEntity->getSerialNumber(), 4, '0', 'before');
         $checkSum = new CheckSum();
         $checkSumEntity = $checkSum->generateSum($iin);
         $iin .= $checkSumEntity->getSum();
