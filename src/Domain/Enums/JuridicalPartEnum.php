53e5c92aba5ca4d197b1c293638581c9a81cbc53
ZnDeveloper <zndev@ya.ru>
2022-06-20 23:35:48 +0600

    upd

--- src/Domain/Enums/JuridicalPartEnum.php
+++ src/Domain/Enums/JuridicalPartEnum.php
@@ -2,7 +2,7 @@
 
 namespace ZnKaz\Iin\Domain\Enums;
 
-use ZnCore\Contract\Enum\Interfaces\GetLabelsInterface;
+use ZnCore\Base\Libs\Enum\Interfaces\GetLabelsInterface;
 
 class JuridicalPartEnum implements GetLabelsInterface
 {
--- src/Domain/Enums/Rbac/IinPermissionEnum.php
+++ src/Domain/Enums/Rbac/IinPermissionEnum.php
@@ -2,7 +2,7 @@
 
 namespace ZnKaz\Iin\Domain\Enums\Rbac;
 
-use ZnCore\Contract\Enum\Interfaces\GetLabelsInterface;
+use ZnCore\Base\Libs\Enum\Interfaces\GetLabelsInterface;
 
 class IinPermissionEnum implements GetLabelsInterface
 {
